738efecbb30bc663237fb53d5e3c44f2a83c91d0
Zara <Zara>
2024-02-21 17:38:11 +0100

    added bg farve, kigget lidt på burgermenu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'blaa': '#B5D7FF',
-                'groen': '#9ED2DD',
-                'textblaa': '#3B2983',
-            },
-            fontFamily: {
-                'jost': ['Jost'],
-            }
-        }
-    }
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    extend: {
+      colors: {
+        blaa: "#B5D7FF",
+        groen: "#9ED2DD",
+        textblaa: "#3B2983",
+        back: "#F5E9E2",
+      },
+      fontFamily: {
+        jost: ["Jost"],
+      },
+    },
+  },
+};
